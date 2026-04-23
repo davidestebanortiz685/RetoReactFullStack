9f429c3fbddc0d7dd393144b7c2e46372c39e61c
esteban <davidestebanortizhernandez@gmail.com>
2026-04-23 18:55:58 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RetoReactFullStack/'
+  base: 'https://davidestebanortiz685.github.io/RetoReactFullStack/'
 })
