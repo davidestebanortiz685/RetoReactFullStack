eed544ef21e6ad1d52a1e3729770298c218d3164
esteban <davidestebanortizhernandez@gmail.com>
2026-04-23 18:42:39 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
